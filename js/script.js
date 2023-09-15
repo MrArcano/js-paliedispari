e7a29a9b471d9f7ac4c10b8d08aa4fdebfedaf68
MrArcano <salvatore-dario@hotmail.it>
2023-09-15 15:20:22 +0200

    [add]: logic palindroma 1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// 1. Salviamo la parola inserita in una variabile
+
+// const parola = prompt("Inserisci una parola:");
+
+// 2. creo un ciclo che cicla per la lunghezza della stringa
+const parola ="anna";
+let parolaNuova = "";
+
+
+// - genero una nuova stringa componendola partendo dai caratteri finali verso l'inizio
+for(i = parola.length -1 ; i >= 0 ; i--){
+  parolaNuova += parola.charAt(i);
+  console.log(parolaNuova);
+}
+
+// - comparo la stringa iniziale con quella nuova, se sono uguali la parola è palindroma e stampo il risultato
+
+if(parola == parolaNuova){
+  console.log("La parole è palindroma");
+}else{
+  console.log("La parola non è palindroma");
+}
